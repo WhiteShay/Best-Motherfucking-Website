4a0513ba2047919335dabcbe313eefc6a2a7f25c
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 02:24:06 +0100

    Booste page: 2eme image

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
-let montitre = document.querySelector('h1');
-montitre.textContent = 'Hello world!';
+let monImage = document.querySelector('img');
+
+monImage.addEventListener('click', function() {
+    let monSrc = monImage.getAttribute('src');
+    if (monSrc === 'images/firefox-icon.png') {
+      monImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      monImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+});
 document.querySelector('html').addEventListener('click', function() {
     alert('Aïe, arrêtez de cliquer !!');
 });
