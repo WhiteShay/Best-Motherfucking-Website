eab8617b4ea35a57e9683a51f46ecb126822a7f7
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 02:42:42 +0100

    Rajout des fonctions actives du bouton

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,4 @@
-let monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
-
 let monImage = document.querySelector('img');
-
 monImage.addEventListener('click', function() {
     let monSrc = monImage.getAttribute('src');
     if (monSrc === 'images/firefox-icon.png') {
@@ -11,6 +7,27 @@ monImage.addEventListener('click', function() {
       monImage.setAttribute('src', 'images/firefox-icon.png');
     }
 }); 
+
+let monBouton = document.querySelector('button');
+let monTitre = document.querySelector('h1')
+
+function définirNomUtilisateur() {
+  let monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+
+if (!localStorage.getItem('nom')) {
+  définirNomUtilisateur();
+} else {
+  let nomEnregistré = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+}
+
+monBouton.addEventListener('click', function() {
+  définirNomUtilisateur();
+});
+
 document.querySelector('html').addEventListener('click', function() {
     alert('Aïe, arrêtez de cliquer !!');
 });
