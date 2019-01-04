e7aa6d8bdf6d0ac70baf4b2fb95354a2db4508eb
WhiteShay <chloe.renard51pro@gmail.com>
2019-01-04 02:14:00 +0100

    Ajout de aïe arrete de cliquer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
 let montitre = document.querySelector('h1');
 montitre.textContent = 'Hello world!';
+document.querySelector('html').addEventListener('click', function() {
+    alert('Aïe, arrêtez de cliquer !!');
+});
